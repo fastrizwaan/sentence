c0228986bf9f3885a4450c556716a961c26cd14b
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2017-08-05 15:45:09 +0530

    Add files via upload

--- sentence.py
+++ sentence.py
@@ -12,50 +12,50 @@ def word(pos):
 
 #Part of Speech (pos)
 noun=['earth','water','fire','air','land','sea','music','flower','tree','dune','snow']
-adje=['pure','clean','serene','wet','sweet','refreshing','hot','cold','dry']
+adj=['pure','clean','serene','wet','sweet','refreshing','hot','cold','dry']
 
 
 #SENTENCE: X is A
-print (word(noun) + " is " + word(adje))
+print (word(noun) + " is " + word(adj))
 
 #SENTENCE: not only THIS but also THAT
 
-adjeOne=word(adje)
-adjeTwo=word(adje)
-# we want different adjective for the second instance
-while adjeOne == adjeTwo:
-	adjeTwo=word(adje)
+adj1=word(adj)
+adj2=word(adj)
+# we want different adjctive for the second instance
+while adj1 == adj2:
+	adj2=word(adj)
 
-print ("not only the " + word(noun) + " is " + adjeOne + " but also " + adjeTwo)
+print ("not only the " + word(noun) + " is " + adj1 + " but also " + adj2)
 
 
 #SENTENCE: if THIS then THAT
 
-nounOne=word(noun)
-nounTwo=word(noun)
+noun1=word(noun)
+noun2=word(noun)
 # we want different noun for the second instance
-while nounOne == nounTwo:
-	nounTwo=word(noun)
+while noun1 == noun2:
+	noun2=word(noun)
 
-print ("if " + nounOne + " is " + adjeOne + " then the " + nounTwo + " is " + adjeTwo)
+print ("if " + noun1 + " is " + adj1 + " then the " + noun2 + " is " + adj2)
 
 # noun is made up of adj1, adj2, adj3 and adj4 noun
 # SENTENCE X is made up of A, B, C, D Y
 
 #reset
-adjeThree=""
-adjeFour=""
-adjeTwo =""
-adjeOne =""
+adj3=""
+adj4=""
+adj2 =""
+adj1 =""
 
 
-while adjeOne == adjeTwo or adjeOne == adjeThree or adjeOne == adjeFour or adjeTwo == adjeThree or adjeTwo == adjeFour or adjeThree == adjeOne or  adjeThree == adjeFour or adjeFour == adjeOne:
+while adj1 == adj2 or adj1 == adj3 or adj1 == adj4 or adj2 == adj3 or adj2 == adj4 or adj3 == adj1 or  adj3 == adj4 or adj4 == adj1:
 
-	adjeThree=word(adje)
-	adjeFour=word(adje)
-	adjeTwo =word(adje)
-	adjeOne =word(adje)
+	adj3=word(adj)
+	adj4=word(adj)
+	adj2 =word(adj)
+	adj1 =word(adj)
 
 
-print (nounOne + " is made up of " + adjeOne +", "+ adjeTwo +", "+ adjeThree +", and " + adjeFour + " " + nounTwo)
+print (noun1 + " is made up of " + adj1 +", "+ adj2 +", "+ adj3 +", and " + adj4 + " " + noun2)
 
